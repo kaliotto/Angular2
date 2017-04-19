ad04b6144c416a3387c134b164005edaccada232
Kaliotto <kaliotto@gmail.com>
2017-04-19 18:56:28 +0200

    Finalización de los formularios (falta algun ejercicio de la aproximación por Data)

--- 08.Formularios/src/app/components/data/data.component.ts
+++ 08.Formularios/src/app/components/data/data.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  formulario: FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      apellido: new FormControl('', Validators.required),
+      correo: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ])
+    });
+  }
+
+  guardar() {
+    console.log("Formulario enviado")
+    console.log(this.formulario)
   }
 
 }
